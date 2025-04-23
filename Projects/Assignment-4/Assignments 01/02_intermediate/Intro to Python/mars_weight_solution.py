570c0d73aee8e6c2e06f2939b9b6a2d15c890072
Waqar Ahmed <waqarahmed786462003@gmail.com>
2025-04-23 16:17:05 +0500

    add intermediate tasks

---
+++ Projects/Assignment-4/Assignments 01/02_intermediate/Intro to Python/mars_weight_solution.py
@@ -0,0 +1,23 @@
+def main():
+
+    planet_gravity = {
+        "Mercury": 0.376,
+        "Venus": 0.889,
+        "Mars": 0.378,
+        "Jupiter": 2.36,
+        "Saturn": 1.081,
+        "Uranus": 0.815,
+        "Neptune": 1.14
+    }
+
+    earth_weight = float(input("Enter a weight on Earth: "))
+    planet = input("Enter a planet: ")
+
+    if planet in planet_gravity:
+        planet_weight = round(earth_weight * planet_gravity[planet], 2)
+        print(f"The equivalent weight on {planet}: {planet_weight}")
+    else:
+        print("Invalid planet name. Please enter a valid planet from the solar system.")
+
+if __name__ == "__main__":
+    main()
