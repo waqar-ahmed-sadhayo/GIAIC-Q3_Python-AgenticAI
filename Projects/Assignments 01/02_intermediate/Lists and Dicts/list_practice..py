5bb99edce610afed6356a17f79d92db433b6c8b2
Waqar Ahmed <waqarahmed786462003@gmail.com>
2025-04-23 12:54:28 +0500

    add list advance tasks

---
+++ Projects/Assignments 01/02_intermediate/Lists and Dicts/list_practice..py
@@ -0,0 +1,9 @@
+
+def main():
+    fruit_list = ['apple', 'banana', 'orange', 'grape', 'pineapple']
+    print("The length of fruit list is: ", len(fruit_list))
+    fruit_list.append("mango")
+    print("The updated fruit list is: ", fruit_list)
+
+if __name__ == "__main__":
+    main()
