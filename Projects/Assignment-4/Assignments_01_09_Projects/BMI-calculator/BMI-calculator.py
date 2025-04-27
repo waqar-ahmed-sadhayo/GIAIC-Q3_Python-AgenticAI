0b3c4f0fcc591bf1cec29c71a777d22d48217f37
Waqar Ahmed <waqarahmed786462003@gmail.com>
2025-04-27 22:32:28 +0500

    Create BMI-calculator.py

---
+++ Projects/Assignment-4/Assignments_01_09_Projects/BMI-calculator/BMI-calculator.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+from tenacity import stop_when_event_set
+
+st.title("BMI Calculator")
+
+height = st.slider("Enter your height (in cm): ", 100, 250, 175)
+weight = st.slider("Enter your weight (in kg): ", 40, 200, 70)
+
+bmi = weight / ((height / 100) ** 2)
+
+st.write(f"Your BMI is: {bmi:.2f}")
+
+st.write("### BMI Categories ###")
+st.write("- Underweight: BMI less than 18.5")
+st.write("- Normal weight: BMI between 18.5 and 24.9")
+st.write("- Overweight: BMI between 25 and 29.9")
+st.write("- Obesity: BMI 30 or greater")
