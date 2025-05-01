0e1b75e16c5a84ab30e2742dca523cb8b284f5fb
Waqar Ahmed <waqarahmed786462003@gmail.com>
2025-05-01 17:20:25 +0500

    Add project

---
+++ Projects/growth-mindset-challenge/app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import numpy as np
+import joblib  # For loading scaler and model
+
+scaler = joblib.load('Scaler.pkl')
+
+model = joblib.load('model.pkl')
+
+st.title("Real Estate Price Prediction App")
+
+st.divider()
+
+bed = st.number_input("Enter the number of bedrooms", value=2, step=1)
+bath = st.number_input("Enter the number of bathrooms", value=1, step=1 )
+size = st.number_input("Enter the size:", value=1000, step=50)
+
+X = [bed, bath, size]
+
+st.divider()
+
+predict_button = st.button("Predict!")
+
+st.divider()
+
+if predict_button:
+
+    st.balloons()
+
+    X1 = np.array(X)
+
+    X_array = scaler.transform([X1])
+
+    prediction = model.predict(X_array)[0]
+
+    st.write(f"The Prediction is: {prediction:.2f}")
+
+else:
+    st.warning("PLease use the button for prediction")
