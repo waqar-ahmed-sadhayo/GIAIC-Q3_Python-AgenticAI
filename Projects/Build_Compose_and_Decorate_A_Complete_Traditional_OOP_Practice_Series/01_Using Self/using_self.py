75ce587d60434922d1fceb4d3581063d258e0302
Waqar Ahmed <waqarahmed786462003@gmail.com>
2025-05-06 12:07:37 +0500

    Create using_self.py

---
+++ Projects/Build_Compose_and_Decorate_A_Complete_Traditional_OOP_Practice_Series/01_Using Self/using_self.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print(f"Student Name: {self.name}")
+        print(f"Marks: {self.marks}")
+
+# Create Object
+student1 = Student("Waqar", 90)
+student1.display()
