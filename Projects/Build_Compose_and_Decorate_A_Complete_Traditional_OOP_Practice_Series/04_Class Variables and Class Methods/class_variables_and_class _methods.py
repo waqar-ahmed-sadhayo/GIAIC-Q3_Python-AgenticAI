b5f4686859b7953df2c65bf4af083f38099b516e
Waqar Ahmed <waqarahmed786462003@gmail.com>
2025-05-07 16:39:39 +0500

    Create class_variables_and_class _methods.py

---
+++ Projects/Build_Compose_and_Decorate_A_Complete_Traditional_OOP_Practice_Series/04_Class Variables and Class Methods/class_variables_and_class _methods.py
@@ -0,0 +1,30 @@
+class Bank:
+     bank_name = "AL Waqar Bank Limited" # Class variable
+
+     def __init__(self, account_holder):
+         self.account_holder = account_holder
+
+     @classmethod
+     def change_bank_name(cls, name):
+         cls.bank_name = name
+
+     def display_info(self):
+        print(f"Account Holder: {self.account_holder}")
+        print(f"Bank: {self.bank_name}")
+
+# Creating instances
+account1 = Bank("Waqar")
+account2 = Bank("Waseem")
+
+# Displaying initial bank name
+print("Before changing bank name")
+account1.display_info()
+account2.display_info()
+
+# Changing bank name using class method
+Bank.change_bank_name("Al Waseem Bank Limited")
+
+# Displaying updated bank name
+print("After changing bank name")
+account1.display_info()
+account2.display_info()
